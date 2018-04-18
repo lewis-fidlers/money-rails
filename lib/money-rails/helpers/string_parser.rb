62f8b0bc0954b49057e4f9649fbe8c2e69a5eefc
Lewis Fidlers <lewisfidlers@gmail.com>
2018-04-18 08:05:22 +0200

    Add string parser which normalized money strings.

---
+++ lib/money-rails/helpers/string_parser.rb
@@ -0,0 +1,40 @@
+class StringParser
+  attr_reader :raw_value, :normalized_raw_value
+
+  def initialize(unparsed_amount)
+    raise "Only takes string as input" unless unparsed_amount.is_a? String
+    @raw_value = unparsed_amount
+    @normalized_raw_value = nil
+  end
+
+  # returns a number in this format: 12000.00
+  def parse
+    strip_unwanted_chars!
+    normalize_raw_value
+  end
+
+  # Leaves only numbers and known separators
+  def strip_unwanted_chars!
+    @raw_value = @raw_value
+      .gsub(/[^\d\.\, ]/, "")
+      .strip
+
+  end
+
+  def normalize_raw_value
+    normalized_integer_part = integer_part.gsub(/(\.|,)/, '')
+    normalized_decimal_part = decimal_part.gsub(/(\.|,)/, '.')
+
+    @normalized_raw_value = "%s%s" % [normalized_integer_part, normalized_decimal_part]
+    @normalized_raw_value.gsub!(/[\s_]/, '')
+    @normalized_raw_value
+  end
+
+  def decimal_part
+    @raw_value.match(/(\.|,)\d{1,2}$/).to_s
+  end
+
+  def integer_part
+    @raw_value.match(/.+?(?=#{Regexp.escape(decimal_part)})/).to_s
+  end
+end
---
+++ spec/helpers/string_parser_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+require 'money-rails/helpers/string_parser'
+
+describe StringParser, :helper do
+
+  locales = %i(en fr nl)
+
+  Money.use_i18n = true
+
+  locales.each do |locale|
+    describe "works with normalized string inputs for #{locale}" do
+
+      around(:each) do |example|
+        I18n.with_locale(locale) do
+          example.run
+        end
+      end
+
+      string_input_permutations = [
+        "1 000 456.50",
+        "1 000 456,50",
+        "1,000,456.50",
+        "1.000.456,50",
+        "1 000 456.5",
+        "1 000 456,5",
+        "1,000,456.5",
+        "1.000.456,5",
+        "1.000.456,50 €",
+        "€ 1,000,456.50",
+      ]
+
+      string_input_permutations.each do |permutation|
+        it "parses #{permutation} to 1000456.50" do
+
+          puts "Current locale = #{I18n.locale}"
+          result = StringParser.new(permutation).parse
+          expect(result.to_f).to eq("1000456.50".to_f)
+        end
+      end
+    end
+  end
+
+  describe ".strip_unwanted_chars" do
+    it "strips everything except digits, points commas and spaces" do
+      parser = StringParser.new("123 456.7x8_9,4!@0")
+      parser.strip_unwanted_chars!
+      expect(parser.raw_value).to eq("123 456.789,40")
+    end
+
+    it "strips everything except digits, points commas and spaces" do
+      parser = StringParser.new("1.000.456,50 €")
+      parser.strip_unwanted_chars!
+      expect(parser.raw_value).to eq("1.000.456,50")
+    end
+  end
+end
